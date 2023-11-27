032921263a944f336e50df7a37b09b7366716965
Chaojian Zhang <szinubuntu@gmail.com>
2023-11-27 15:19:22 -0500

    Draft

---
+++ Snippets/Markdown/DocumentParse.cs
@@ -0,0 +1,5 @@
+/* Document Parse
+Verion: v0.0.0
+
+Parse markdown document into hierarchical components instead of as sequence of blocks, which will then make it trivial to re-organize things.
+*/
---
+++ Snippets/Markdown/SortSections.cs
@@ -0,0 +1,7 @@
+/* Sort Sections
+Version: v0.0.0
+
+Use regular expression to sort entire sections at a given level under some parent section within markdown document.
+*/
+
+// PENDING
