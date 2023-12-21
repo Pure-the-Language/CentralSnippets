fdf284143f3f8435a9f63375225ece8f92fc7be9
Chaojian Zhang <szinubuntu@gmail.com>
2023-12-21 11:40:59 -0500

    Draft

---
+++ Snippets/KMD/GatherDatedFolders.cs
@@ -0,0 +1,10 @@
+/* Gather Dated Folders
+Version: v0.1
+
+This snippet gathers all folders on current computer following `` scheme, using Everything service.
+This snippet depends on Everything service, proper Everything service configuration (exclusions etc), and NewWorldFoundation's [EverythingService module](https://github.com/Charles-Zhang-Experiments/NewWorldFoundation/tree/main/Solution/NewWorldFoundation/Shared/EverythingService) (made available in PATH).
+
+Changelog:
+* Version 0.1: Initial setup.
+*/
+
