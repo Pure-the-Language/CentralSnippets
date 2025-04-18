1cfebe65d88183646af1d507ae0826f8d276993b
Charles Zhang <szinubuntu@gmail.com>
2025-04-18 06:55:35 -0400

    Draft

--- Snippets/Markdown/DocumentParse.cs
+++ Snippets/Markdown/DocumentParse.cs
@@ -1,5 +1,36 @@
 /* Document Parse
-Verion: v0.0.0
+Version: v0.0.0
 
-Parse markdown document into hierarchical components instead of as sequence of blocks, which will then make it trivial to re-organize things.
+Parse markdown document into hierarchical components instead of as sequence of blocks, which will then make it trivial to re-organize things. We do not make use of existing libraries for maximum compatibility, lightweight-ness, and a more reasonable level of abstraction. This file is self-contained and a single file. (Probably should publish this)
+For more advanced implementation, consult Parcel NExT DSL.
 */
+
+public enum MarkdownBlockType
+{
+    Paragraph,
+    Bullets,
+    Image,
+    Header,
+    CodeBlock,
+    Footnotes,
+    Table,
+    BlockQuote,
+    HTML
+}
+public class MarkdownBlock
+{
+
+}
+public class HierarchicalMarkdownBlock: MarkdownBlock
+{
+
+}
+
+string filePath = Arguments[0];
+
+MarkdownBlock[] blocks = ParseBlocks(File.ReadLines(filePath));
+
+MarkdownBlock[] ParseBlocks(IEnumerable<string> lines)
+{
+
+}
